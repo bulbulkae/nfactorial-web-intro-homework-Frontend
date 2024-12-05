9054b22535639b80295b1f8324a17e2e30fe6b7f
Bulbul Yergalikyzy Kasymbek <b_kasymbek@kbtu.kz>
2024-12-05 16:39:30 +0500

    hw5

--- homeworks/hw5/index.js
+++ homeworks/hw5/index.js
@@ -14,3 +14,8 @@ console.log(document.querySelectorAll('input')[0]);
 // Последний input в этой форме.
 let res = document.querySelectorAll('input');
 console.log(res[res.length - 1]);
+
+let body = document.body;
+body.innerHTML = '<!--' + body.tagName + '-->'; // содержимое элемента <body> to change line as comment with tag name BODY
+console.log(body.firstChild.data); // что выведет?
+// we changed the content of the body to comment "BODY" and then here we try to get first Child of the <body> .data will return содержимое то есть "BODY"
